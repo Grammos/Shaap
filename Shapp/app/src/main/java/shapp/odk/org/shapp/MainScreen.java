db52b60e95d795d92c7be0023bd9718b5163e4b3
Grammos <kamberigramos@gmail.com>
2015-06-22 16:25:10 +0200

    #22 Main Screen: Add background images to the buttons.

--- Shapp/app/src/main/java/shapp/odk/org/shapp/MainScreen.java
+++ Shapp/app/src/main/java/shapp/odk/org/shapp/MainScreen.java
@@ -1,57 +1,119 @@
 package shapp.odk.org.shapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainScreen extends Activity implements View.OnClickListener {
+public class MainScreen extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
+        GridView gridView = (GridView)findViewById(R.id.gridview);
+        gridView.setAdapter(new MyAdapter(this));
+
+        /**
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ImageView item = (ImageView) view.setOnClickListener();
+            }
+        });*/
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main_screen, menu);
-        return true;
+    private class MyAdapter extends BaseAdapter
+    {
+        private List<Item> items = new ArrayList<Item>();
+        private LayoutInflater inflater;
+
+        public MyAdapter(Context context)
+        {
+            inflater = LayoutInflater.from(context);
+
+            items.add(new Item("Image 1", R.drawable.nature1));
+            items.add(new Item("Image 2", R.drawable.nature2));
+            items.add(new Item("Image 3", R.drawable.tree1));
+            items.add(new Item("Image 4", R.drawable.nature3));
+            items.add(new Item("Image 5", R.drawable.tree2));
+            items.add(new Item("Image 6", R.drawable.nature1));
+
+
+
+        }
+
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int i)
+        {
+            return items.get(i);
+        }
+
+        @Override
+        public long getItemId(int i)
+        {
+            return items.get(i).drawableId;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup)
+        {
+            View v = view;
+            ImageView picture;
+            TextView name;
+
+            if(v == null)
+            {
+                v = inflater.inflate(R.layout.gridview_item, viewGroup, false);
+                v.setTag(R.id.picture, v.findViewById(R.id.picture));
+            }
+
+            picture = (ImageView)v.getTag(R.id.picture);
+
+            Item item = (Item)getItem(i);
+
+            picture.setImageResource(item.drawableId);
+
+
+            return v;
+        }
+
+        private class Item
+        {
+            final String name;
+            final int drawableId;
+
+            Item(String name, int drawableId)
+            {
+                this.name = name;
+                this.drawableId = drawableId;
+            }
+        }
+
     }
 
 
 
-    @Override
-    public void onClick(View view) {
-
-    }
-
-    public void button1(View view) {
-        Log.d("Button1", "Report");
-    }
-
-    public void button2(View view) {
-        Log.d("Button2", "Light");
-    }
-
-    public void button3(View view) {
-        Log.d("Button3", "Maps");
-    }
-
-    public void button4(View view) {
-        Log.d("Button4", "Analytics");
-    }
-
-    public void button5(View view) {
-        Log.d("Button5", "Settings");
-    }
-
-    public void button6(View view) {
-        Log.d("Button1", "Contact");
-
-    }
 }
