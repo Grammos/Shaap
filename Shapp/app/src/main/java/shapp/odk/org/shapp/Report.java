eb65cb7ed7a1e6476f2720f5f9a1306597153835
Partini <partin.imeri@gmail.com>
2015-07-19 21:30:09 +0200

    Report Screen: Extract hardcoded json array for harassment types into API

--- Shapp/app/src/main/java/shapp/odk/org/shapp/Report.java
+++ Shapp/app/src/main/java/shapp/odk/org/shapp/Report.java
@@ -3,11 +3,11 @@ package shapp.odk.org.shapp;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
-
 
 import com.squareup.picasso.Picasso;
 
@@ -15,9 +15,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class Report extends Activity {
 
@@ -40,7 +44,6 @@ public class Report extends Activity {
 
     }
 
-
     /**
      * should consider implementing cache manager for better performance
      *
@@ -54,21 +57,49 @@ public class Report extends Activity {
         public ReportAdapter(Context contextReport)
         {
             this.contextReport = contextReport;
-            // Ensure we get a different ordering of images on each run.
-
-            //urlsButtonPics.add("https://forums.digitalpoint.com/proxy/V%2FlNRoM4xGeK5YYhZVYcS%2B9mKLD4rQceAt2HpNNZGKK%2BHZpMfoiRYQ7ix2kpUgWra42tnY7BAVsNJQ7S%2FjO7x9qBnsoK7JVSpOe4p9UJ6N8%2FcgLNOQ%3D%3D/image.png");
-            //urlsButtonPics.add("https://forums.digitalpoint.com/proxy/V%2FlNRoM4xGeK5YYhZVYcS%2B9mKLD4rQceAt2HpNNZGKK%2BHZpMfoiRYQ7ix2kpUgWra42tnY7BAVsNJQ7S%2FjO7x9qBnsoK7JVSpOe4p9UJ6N8%2FcgLNOQ%3D%3D/image.png");
-
-            String harassmentJsonString = "[{\"imageUrl\": \"https://placeholdit.imgix.net/~text?txtsize=33&txt=Verbal&w=300&h=300\", \"id\": \"verbal\", \"name\": \"Verbal\"}," +
-                    "{\"imageUrl\": \"https://placeholdit.imgix.net/~text?txtsize=33&txt=Stalking&w=300&h=300\", \"id\": \"stalking\", \"name\": \"Stalking\"},"+
-                    "{\"imageUrl\": \"https://placeholdit.imgix.net/~text?txtsize=33&txt=Groping&w=300&h=300\", \"id\": \"groping\", \"name\": \"Groping\"},"+
-                    "{\"imageUrl\": \"https://placeholdit.imgix.net/~text?txtsize=33&txt=Assault&w=300&h=300\", \"id\": \"assault\", \"name\": \"Assault\"},"+
-                    "{\"imageUrl\": \"https://placeholdit.imgix.net/~text?txtsize=33&txt=Flashing&w=300&h=300\", \"id\": \"flashing\", \"name\": \"Flashing\"},"+
-                    "{\"imageUrl\": \"https://placeholdit.imgix.net/~text?txtsize=33&txt=Racism&w=300&h=300\", \"id\": \"racism\", \"name\": \"Racism\"}]" ;
 
             try {
 
-                JSONArray harassmentJsonArray= new JSONArray(harassmentJsonString);
+                //it's not a good choice i don't want to cheat on the "network" UI of the thread
+                if (android.os.Build.VERSION.SDK_INT > 9) {
+                    StrictMode.ThreadPolicy policy =
+                            new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+                }
+
+
+                URL urlHarassmentApi = new URL("http://10.0.2.2:5000/geo-api");
+                HttpURLConnection connReport = (HttpURLConnection)urlHarassmentApi.openConnection();
+
+                //add request header
+                connReport.setRequestProperty("Accept", "application/json");
+
+                connReport.setRequestMethod("GET");
+
+                //int responseCode = connReport.getResponseCode();
+
+                if (connReport.getResponseCode() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : "
+                            + connReport.getResponseCode());
+                }
+
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(connReport.getInputStream()));
+
+                String inputLine;
+
+
+                StringBuffer response = new StringBuffer();
+
+
+                while ((inputLine = reader.readLine()) != null) {
+                    response.append(inputLine);
+
+                }
+
+                reader.close();
+
+                JSONArray harassmentJsonArray= new JSONArray(response.toString());
 
                 for (int i = 0; i < harassmentJsonArray.length(); i++) {
 
@@ -76,27 +107,17 @@ public class Report extends Activity {
                     String imageUrl = harassment.getString("imageUrl");
                     String harassmentName =  harassment.getString("name");
                     String harassmentId = harassment.getString("id");
+
                     urlsButtonPics.add(imageUrl);
-
-                    //Log.d("hello",""+ imageUrl);
-                    //Log.d("hello",""+ harassmentName);
-                    //Log.d("hello",""+ harassmentId);
-
 
                 }
 
-                //urlsButtonPics.add(harassmentJsonString);
 
 
-            } catch (JSONException e) {
+            } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
 
-
-            // Triple up the list.
-            //ArrayList<String> copy = new ArrayList<String>(urlsButtonPics);
-            //.addAll(copy);
-            //urlsButtonPics.addAll(copy);
         }
 
 
@@ -123,8 +144,6 @@ public class Report extends Activity {
                 view = new CircledImageView(contextReport);
 
             }
-
-
 
             // Get the image URL for the current position.
             String url = getItem(position);
